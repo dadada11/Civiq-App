7dba85875b9f93492eb4e36ef33a301d0f111704
vivek <vivekn289@gmail.com>
2017-08-02 22:53:34 -0700

    trying google authentication

--- app/src/main/java/com/civiqapp/civiqapp/MainActivity.java
+++ app/src/main/java/com/civiqapp/civiqapp/MainActivity.java
@@ -1,56 +1,38 @@
 package com.civiqapp.civiqapp;
 
-import android.support.annotation.NonNull;
+import android.app.ProgressDialog;
+import android.support.annotation.VisibleForTesting;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import android.app.ProgressDialog;
+import android.support.annotation.VisibleForTesting;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    public FirebaseAuth mAuth;
-    public FirebaseAuth.AuthStateListener mAuthListener;
+    @VisibleForTesting
+    public ProgressDialog mProgressDialog;
 
-    protected void onCreate(Bundle savedInstanceState) {
-        final FirebaseAuth mAuth;
-        final FirebaseAuth.AuthStateListener mAuthListener;
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+           // mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mAuth = FirebaseAuth.getInstance();
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    // User is signed in
-                    Log.d("authmessagething", "onAuthStateChanged:signed_in:" + user.getUid());
-                } else {
-                    // User is signed out
-                    Log.d("authmessagethingelse", "onAuthStateChanged:signed_out");
-                }
-                // ...
-            }
-        };
-
-
+        mProgressDialog.show();
     }
-    @Override
-    public void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        if (mAuthListener != null) {
-            mAuth.removeAuthStateListener(mAuthListener);
-        }
+        hideProgressDialog();
     }
- }
+
+}
